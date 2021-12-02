c3dccc8e573cda90f3ba36603684026d157150fd
Manuel Tejado Morilla <manueltejmor@gmail.com>
2021-12-02 09:22:17 +0100

    array

--- src/com/salesianostriana/dam/Main.java
+++ src/com/salesianostriana/dam/Main.java
@@ -14,5 +14,18 @@ public class Main {
         System.out.println(a);
 
         System.out.println("Hello World");
+
+        System.out.println("Hola mundo");
+
+        int[] array = new int[3];
+        array[0] = 123;
+        array[1] = 456;
+        array[2] = 789;
+
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+
+
     }
 }
